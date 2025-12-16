7f43edfbcbbc093eea2097000aa2312144d486dc
Rupesh Chincholkar <rupeshchincholkar14@gmail.com>
2025-12-16 18:24:20 +0530

    route group, shadcn setup

---
+++ src/components/Navbar.js
@@ -0,0 +1,10 @@
+import { Button } from "@/components/ui/button"
+
+export default function Navbar(){
+    return(
+        <nav>
+            Navbar
+            <Button>about</Button>
+        </nav>
+    )
+}
